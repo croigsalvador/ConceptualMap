b60122370fc01c65ef248b5c0779013f39e176ed
roxeman3 <roxeman3@gmail.com>
2014-07-04 18:44:01 +0200

    Initial Commit

---
+++ ConceptualMap/CRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRAppDelegate.h
+//  ConceptualMap
+//
+//  Created by Carlos Roig Salvador on 04/07/14.
+//  Copyright (c) 2014 CRoigSalvador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
